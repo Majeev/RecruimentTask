9fb992f433dd9b184e6a7c0a47a31d419f858b14
Jakub Majewski <majewski.j01@gmail.com>
2022-06-16 19:20:03 +0200

    fix: cleaning code a bit

--- app.js
+++ app.js
@@ -26,37 +26,29 @@ const students = [
   },
 ];
 
-for (let student of students) {
-  let average = 0;
-  const filteredMarks = student.marks.filter(
-    (mark) => typeof mark === "number"
-  );
-  for (let mark of filteredMarks) {
-    average += mark;
-  }
-  average = average / filteredMarks.length;
-}
-
 const student = students
   .map((info) => {
     const { name, lastName, marks } = info;
     const filteredMarks = marks.filter((mark) => typeof mark === "number");
+
     const reducedMarks = filteredMarks.reduce(function (
       previousValue,
       currentValue
     ) {
       return previousValue + currentValue;
     });
+
     const averageMark = reducedMarks / marks.length;
 
     return `
-  <div class="${averageMark > 5 ? "fiveAndOver" : "belowFive"}">
-    <p>Name: ${name}</p>
-    <p>Last Name: ${lastName}</p>
-    <p>Attendance: ${filteredMarks.length} lectures</p>
-    <p>Average mark: ${averageMark}
- </div>
+      <div class="${averageMark > 5 ? "fiveAndOver" : "belowFive"}">
+        <p>Name: ${name}</p>
+        <p>Last Name: ${lastName}</p>
+        <p>Attendance: ${filteredMarks.length} lectures</p>
+        <p>Average mark: ${averageMark}
+    </div>
 `;
   })
   .join("");
+
 wrapper.innerHTML = student;
